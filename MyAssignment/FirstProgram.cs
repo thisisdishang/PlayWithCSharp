97b5db591290d8de1c9f79edd8cd5fa0c0375a3b
thisisdishang <dishangkumarrana@gmail.com>
2024-08-30 21:25:44 +0530

    Add types of variables and check difference between Write and WriteLine method

--- MyAssignment/FirstProgram.cs
+++ MyAssignment/FirstProgram.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace FirstProgram
+namespace PlayWithCSharp
 {
     class First
     {
         static void Main(string[] args)
         {
             Console.WriteLine("First C Sharp Program");
+            Console.WriteLine("New Line Added");
+            Console.WriteLine("a" + "b" + "C");
+            Console.WriteLine(9 + 10 + 5);
+            Console.Write("Statement without newline...");
+            Console.Write("New Statement");
+            // Single line comment
+            /* Multi line comment */
         }
     }
 }
---
+++ MyAssignment/Variables.cs
@@ -0,0 +1,54 @@
+﻿// variables in c sharp
+
+using System;
+
+namespace PlayWithCSharp
+{
+    internal class Variables
+    {
+        static void Main(string[] args)
+        {
+            int a = 50;
+            Console.WriteLine(a);
+
+            // note that if you assign a new value to an existing variable, it will overwrite the previous value
+            a = 75;
+            Console.WriteLine(a);
+
+            string name = "Lucifer";
+            Console.WriteLine("Hello "+name);
+
+            char letter= 'D';
+            Console.WriteLine(letter);
+
+            double e = 7.2D;
+            Console.WriteLine(e);
+
+            bool result= true;
+            Console.WriteLine(result);
+
+            // constant variable
+            const double PI = 3.14D;
+            Console.WriteLine(PI);
+
+            // add two variable to another variable
+            string firstName = "Tony ";
+            string lastName = "Stark ";
+            string fullName = firstName + lastName;
+            Console.WriteLine(fullName);
+
+            int x = 10;
+            int y = 14;
+            Console.WriteLine(x+y);
+
+            // declare many variables
+            int p = 15, q = 20, r = 25;
+            Console.WriteLine(p+q+r);
+
+            int s, t, u;
+            s = t = u = 7;
+            Console.WriteLine(s+t+u);
+
+        }
+    }
+}
