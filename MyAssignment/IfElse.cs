9fa76b9801114d5009448bda719da416d3b536f2
Dishang H. Rana <dishangkumarrana@gmail.com>
2024-09-05 23:04:47 +0530

    Update IfElse.cs

--- MyAssignment/IfElse.cs
+++ MyAssignment/IfElse.cs
@@ -7,6 +7,12 @@ namespace PlayWithCSharp
     {
         static void Main(string[] args)
         {
+          if(age>18){
+          Console.WriteLine("You're eligible for vote");
+          else{
+Console.WriteLine("You're not eligible for vote");
+}
+        }
 
         }
     }
