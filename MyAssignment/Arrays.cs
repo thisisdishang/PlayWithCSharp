384666600403b37af5d293aebae9fed91565b437
thisisdishang <dishangkumarrana@gmail.com>
2024-09-08 14:51:40 +0530

    Add Arrays.cs

---
+++ MyAssignment/Arrays.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace PlayWithCSharp
+{
+    internal class Arrays
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Alpha","Beta","Gama"};
+            int[] no= {1,2,3,4,5,6,7,8,9,10};
+
+            foreach (var item in no)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            // access the elements of an array
+            Console.WriteLine(names[2]);
+            names[2] = "Ela";
+            Console.WriteLine(names[2]);
+
+            // array length
+            Console.WriteLine(names.Length);
+
+            // other ways to create an array
+            string[] car;
+            car= new string[] {"Volvo","BMW","Ford"};
+        }
+    }
+}
