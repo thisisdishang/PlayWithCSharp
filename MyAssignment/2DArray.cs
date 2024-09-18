8e90f34f460d2b739b26eb7b18c3e727cbf88615
thisisdishang <dishangkumarrana@gmail.com>
2024-09-18 20:12:41 +0530

    Implement multidimensional arrays

---
+++ MyAssignment/2DArray.cs
@@ -0,0 +1,47 @@
+﻿// Multidimensional Arrays
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayWithCSharp
+{
+    internal class _2DArray
+    {
+        static void Main(string[] args)
+        {
+            // The single comma [,] specifies that the array is two-dimensional
+            int[,] my2Darray = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            // Second row first column
+            Console.WriteLine(my2Darray[1, 0]);
+
+            my2Darray[1, 1] = 7;
+            Console.WriteLine(my2Darray[1, 1]);
+
+            // A three-dimensional array would have two commas: int[,,]
+            int[,,] my3Darray = { { { 2, 4, 6 }, { 8, 10, 12 } }, { { 1, 3, 5 }, { 7, 9, 11 } } };
+
+            // Loop through 2D array
+            foreach (int i in my2Darray)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Using for loop
+            Console.WriteLine("\nDisplay 3D Array:-");
+            for (int i = 0; i < my3Darray.GetLength(0); i++)
+            {
+                for (int j = 0; j < my3Darray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < my3Darray.GetLength(2); k++)
+                    {
+                        Console.WriteLine(my3Darray[i, j, k]);
+                    }
+                }
+            }
+        }
+    }
+}
--- MyAssignment/Arrays.cs
+++ MyAssignment/Arrays.cs
@@ -7,16 +7,11 @@ namespace PlayWithCSharp
         static void Main(string[] args)
         {
             string[] names = { "Alpha","Beta","Gama","Delta"};
-            int[] no= {1,2,3,4,5,6,7,8,9,10};
-
-            foreach (var item in no)
-            {
-                Console.WriteLine(item);
-            }
-
-
+         
             // access the elements of an array
             Console.WriteLine(names[2]);
+
+            // change an array element
             names[2] = "Ela";
             Console.WriteLine(names[2]);
 
@@ -26,6 +21,46 @@ namespace PlayWithCSharp
             // other ways to create an array
             string[] car;
             car= new string[] {"Volvo","BMW","Ford"};
+
+            // loop through an array
+            for(int i = 0; i < car.Length; i++)
+            {
+                Console.WriteLine(car[i]);
+            }
+            
+            // add element in existing array
+            car=car.Append("Mercedes").ToArray();
+            car = car.Concat(["Audi", "Ferrari"]).ToArray();
+            
+            // foreach loop
+            foreach (string item in car)
+            {
+                Console.WriteLine(item);
+            }
+
+            // sort string array
+            Array.Sort(car);
+            
+            Console.WriteLine("\nSorted Array:-");
+            
+            foreach(string item in car)
+            {
+                Console.WriteLine(item);
+            }
+
+            int[] no = {89,12,0,63,4,9,3,1,10 };
+
+            // sort int array
+            Array.Sort(no);
+
+            foreach (var item in no)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Maximum Number: "+no.Max());
+            Console.WriteLine("Minimum Number: " + no.Min());
+            Console.WriteLine("Sum of Element: " + no.Sum());
         }
     }
 }
