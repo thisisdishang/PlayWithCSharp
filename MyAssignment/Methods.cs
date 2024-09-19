5f01fbcddb0ff807518a150bada2d185d8e0c77d
thisisdishang <dishangkumarrana@gmail.com>
2024-09-19 20:51:06 +0530

    Add all types of methods

---
+++ MyAssignment/Methods.cs
@@ -0,0 +1,56 @@
+﻿// Methods in C Sharp
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayWithCSharp
+{
+    class Methods
+    {
+        // create a method
+        static void MyMethod()
+        {
+            Console.WriteLine("Method is call");
+        }
+
+        // method parameter
+        static void MyMethod2(string name, string role)
+        {
+            Console.WriteLine("Welcome " + name);
+            Console.WriteLine("Your role is " + role);
+        }
+
+        // default parameter
+        static int square(int num = 0)
+        {
+            return num * num;
+        }
+
+        // return values
+        static int sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        // named arguments
+        static void MyMethod3(string child1, string child2, string child3)
+        {
+            Console.WriteLine("The youngest child is: " + child3);
+        }
+
+
+        static void Main(string[] args)
+        {
+            // call a method
+            MyMethod();
+            MyMethod2("Lucy", "Student");
+            Console.WriteLine("Squre of number: " + square(5));
+            Console.WriteLine("Squre of number: " + square());
+            Console.WriteLine("Sum of two numbers: " + sum(5, 2));
+            MyMethod3(child3: "Amy", child1: "Lais", child2: "Reya");
+        }
+    }
+}
