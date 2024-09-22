44bdf64fd7355854408e609a56716ed35e3e8608
thisisdishang <dishangkumarrana@gmail.com>
2024-09-22 19:10:20 +0530

    Start the OOP in C Sharp

---
+++ MyAssignment/OOP/ClassObject.cs
@@ -0,0 +1,21 @@
+﻿// Class and Object in C Sharp
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssignment.OOP
+{
+    class Car
+    {
+        public string color = "Red"; // field
+
+        static void Main(string[] args)
+        {
+            Car ob1 = new Car();
+            Console.WriteLine(ob1.color);
+        }
+    }
+}
---
+++ MyAssignment/OOP/Second.cs
@@ -0,0 +1,19 @@
+﻿// Access properties from another class
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssignment.OOP
+{
+    internal class Second
+    {
+        static void Main(string[] args)
+        {
+            Car ob1 = new Car();
+            Console.WriteLine(ob1.color);
+        }
+    }
+}
