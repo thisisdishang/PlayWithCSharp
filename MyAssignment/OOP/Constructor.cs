162c776ee08540a0bcaa1b0c4f6bd784c61186e4
Dishang H. Rana <dishangkumarrana@gmail.com>
2024-09-29 23:10:19 +0530

    Update Constructor.cs

--- MyAssignment/OOP/Constructor.cs
+++ MyAssignment/OOP/Constructor.cs
@@ -1,1 +1,18 @@
-// Constructor in C Sharp
+// Constructor in C Shar
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssignment.OOP
+{
+    internal class Constructor
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
