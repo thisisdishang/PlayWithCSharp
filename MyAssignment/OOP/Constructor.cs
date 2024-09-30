717ef4a4f0fbad55050ea1c70a690ca044f8ef5e
Dishang H. Rana <dishangkumarrana@gmail.com>
2024-09-30 23:32:12 +0530

    Update Constructor.cs

--- MyAssignment/OOP/Constructor.cs
+++ MyAssignment/OOP/Constructor.cs
@@ -8,11 +8,20 @@ using System.Threading.Tasks;
 
 namespace MyAssignment.OOP
 {
-    internal class Constructor
-    {
-        static void Main(string[] args)
-        {
-           
-        }
-    }
+    class Car
+{
+  public string model;  // Create a field
+
+  // Create a class constructor for the Car class
+  public Car()
+  {
+    model = "Mustang"; // Set the initial value for model
+  }
+
+  static void Main(string[] args)
+  {
+    Car Ford = new Car();  // Create an object of the Car Class (this will call the constructor)
+    Console.WriteLine(Ford.model);  // Print the value of model
+  }
+}
 }
