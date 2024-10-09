838af93290d68c6c96dfd815a14dd119f84a4dd4
thisisdishang <dishangkumarrana@gmail.com>
2024-10-09 20:08:20 +0530

    Update the Constructor.cs

--- MyAssignment/OOP/Constructor.cs
+++ MyAssignment/OOP/Constructor.cs
@@ -11,6 +11,7 @@ namespace MyAssignment.OOP
     internal class Car2
     {
         public string model;  // Create a field
+        public string modelcolor;
 
         // Create a class constructor for the Car class
         public Car2()
@@ -18,9 +19,17 @@ namespace MyAssignment.OOP
             model = "Mustang"; // Set the initial value for model
         }
 
+        // parameterize constructor
+        public Car2(string modelcolor)
+        {
+            this.modelcolor = modelcolor;
+        }
+
         static void Main(string[] args)
         {
             Car2 Ford = new Car2();  // Create an object of the Car Class (this will call the constructor)
+            Car2 BMW = new Car2("white");
+            Console.WriteLine(BMW.modelcolor);
             Console.WriteLine(Ford.model);  // Print the value of model
         }
     }
