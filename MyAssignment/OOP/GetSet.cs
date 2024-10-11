67b520bd7259b30af8d1cedbf4e733e0f0b9a26e
thisisdishang <dishangkumarrana@gmail.com>
2024-10-11 22:25:52 +0530

    Implement properties using get set method

---
+++ MyAssignment/OOP/GetSet.cs
@@ -0,0 +1,30 @@
+﻿// Properties in C Sharp
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssignment.OOP
+{
+    class Person
+    {
+        private string name;
+        public string Name { get { return name; } set { name = value; } }
+
+        public int no { get; set; }
+    }
+    internal class GetSet
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Demon";
+            Console.WriteLine(person.Name);
+            person.no = 7;
+            Console.WriteLine(person.no);
+        }
+    }
+
+}
