6d3218cd56d4c41a42e2ffe74c1b1b026dddbd14
thisisdishang <dishangkumarrana@gmail.com>
2024-10-13 19:58:07 +0530

    Implement polymorphism

---
+++ MyAssignment/OOP/Polymorphism.cs
@@ -0,0 +1,18 @@
+﻿// Polymorphism in C Sharp
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssignment.OOP
+{
+    internal class Polymorphism
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
