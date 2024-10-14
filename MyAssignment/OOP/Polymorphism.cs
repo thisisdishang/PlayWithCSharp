2c43bf943e4f0d227f9a48ba34b6f121eab15786
thisisdishang <dishangkumarrana@gmail.com>
2024-10-14 23:14:35 +0530

    Update Polymorphism.cs

--- MyAssignment/OOP/Polymorphism.cs
+++ MyAssignment/OOP/Polymorphism.cs
@@ -8,11 +8,42 @@ using System.Threading.Tasks;
 
 namespace MyAssignment.OOP
 {
+
+    class RBI
+    {
+        public virtual void rate()
+        {
+            Console.WriteLine("Rate Interest in RBI 8%");
+        }
+    }
+
+    class SBI : RBI
+    {
+        public override void rate()
+        {
+            Console.WriteLine("Rate Interest in SBI 10%");
+        }
+    }
+
+    class HDFC : RBI
+    {
+        public override void rate()
+        {
+            Console.WriteLine("Rate Interest in HDFC 12%");
+        }
+    }
+
     internal class Polymorphism
     {
         static void Main(string[] args)
         {
+            RBI o1 = new RBI();
+            RBI o2 = new SBI();
+            RBI o3 = new HDFC();
 
+            o1.rate();
+            o2.rate();
+            o3.rate();
         }
     }
 }
