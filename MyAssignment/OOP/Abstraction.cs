a52e5cc05d5618366120727e69c3cefbff822228
thisisdishang <dishangkumarrana@gmail.com>
2024-10-17 23:37:39 +0530

    Update Abstraction.cs

--- MyAssignment/OOP/Abstraction.cs
+++ MyAssignment/OOP/Abstraction.cs
@@ -8,8 +8,37 @@ using System.Threading.Tasks;
 
 namespace MyAssignment.OOP
 {
+    abstract class Shape
+    {
+        public abstract void area();
+        public virtual void color()
+        {
+            Console.WriteLine("Red");
+        }
+    }
+
+    class circle : Shape
+    {
+        public override void area()
+        {
+            Console.WriteLine("PI*r*r");
+        }
+
+        public override void color()
+        {
+            Console.WriteLine("Blue");
+        }
+
+    }
+
+
     class Abstraction
     {
-
+        static void main(string[] args)
+        {
+            circle ob1 = new circle();
+            ob1.area();
+            ob1.color();
+        }
     }
 }
