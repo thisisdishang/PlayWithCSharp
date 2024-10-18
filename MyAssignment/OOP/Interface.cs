3a56fbf182a2180aed1658591a9b775f50d8fd01
thisisdishang <dishangkumarrana@gmail.com>
2024-10-18 23:50:42 +0530

    Implement interface

---
+++ MyAssignment/OOP/Interface.cs
@@ -0,0 +1,14 @@
+﻿// Interface in C Sharp
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssignment.OOP
+{
+    class Interface
+    {
+    }
+}
