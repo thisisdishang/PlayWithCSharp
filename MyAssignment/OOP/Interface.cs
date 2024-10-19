c46bb95e879ea5e2f652bffca2a2c47e3dce045b
thisisdishang <dishangkumarrana@gmail.com>
2024-10-19 23:46:25 +0530

    Update interface.cs

--- MyAssignment/OOP/Interface.cs
+++ MyAssignment/OOP/Interface.cs
@@ -8,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace MyAssignment.OOP
 {
+    interface Bank
+    {
+        void interest();
+    }
+
+    class Kotak : Bank
+    {
+        public void interest()
+        {
+            Console.WriteLine("SBI Interest:8%");
+        }
+    }
+
     class Interface
     {
+        static void Main(string[] args)
+        {
+            Kotak s1 = new Kotak();
+            s1.interest();
+        }
     }
 }
