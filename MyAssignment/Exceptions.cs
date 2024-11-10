4cb33d253b0916602a3b143233a7dea333c124b2
thisisdishang <dishangkumarrana@gmail.com>
2024-11-10 17:19:38 +0530

    Implement Files Handling and Exception Handling

---
+++ MyAssignment/Exceptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssignment
+{
+    internal class Exceptions
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] numbers = { 1, 2, 3, 4, 5 };
+                Console.WriteLine(numbers[10]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("finally block is executed");
+            }
+        }
+    }
+}
---
+++ MyAssignment/Files.cs
@@ -0,0 +1,22 @@
+﻿// Files in C Sharp
+
+using System.IO;
+
+namespace MyAssignment
+{
+    internal class Files
+    {
+        static void Main(string[] args)
+        {
+            string writeText = "Never Settle";
+            File.WriteAllText("myfile.txt", writeText);
+
+            string appendText = "For Less";
+            File.AppendAllText("myfile.txt", appendText);
+
+            string readText = File.ReadAllText("myfile.txt");
+            Console.WriteLine(readText);
+
+        }
+    }
+}
