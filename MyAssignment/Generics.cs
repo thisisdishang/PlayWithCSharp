40db0c8dfe301d9142878537b285e8fcfd21e85f
thisisdishang <dishangkumarrana@gmail.com>
2024-11-11 22:56:48 +0530

    Update Generics.cs

--- MyAssignment/Generics.cs
+++ MyAssignment/Generics.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Generics in C Sharp
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,35 @@ using System.Threading.Tasks;
 
 namespace MyAssignment
 {
+
+    // Generic Class
+    class GenericsClass<T>
+    {
+        public GenericsClass(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
+    class NonGenericsClass
+    {
+        // Generic Method
+        public void Show<T>(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
     internal class Generics
     {
+        static void Main(string[] args)
+        {
+            GenericsClass<string> gen = new GenericsClass<string>("Legend Never Dies");
+            GenericsClass<int> gen2 = new GenericsClass<int>(7);
+            GenericsClass<char> gen3 = new GenericsClass<char>('D');
+            NonGenericsClass nonGenericsClass = new NonGenericsClass();
+            nonGenericsClass.Show("This is generic method");
+            nonGenericsClass.Show(101);
+        }
     }
 }
