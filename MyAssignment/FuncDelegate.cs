9cebf108d586dce31d9b206945fafba6bde4fa9d
thisisdishang <dishangkumarrana@gmail.com>
2024-11-17 18:45:01 +0530

    Implement Func Delegate

---
+++ MyAssignment/FuncDelegate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssignment
+{
+
+    internal class FuncDelegate
+    {
+        public static int FuncMethod(int a, int b, int c, int d)
+        {
+            return a + b + c + d;
+        }
+
+        static void Main(string[] arg)
+        {
+            Func<int, int, int, int,int> fd = FuncMethod;
+            Console.WriteLine("Func Delegate: "+fd(4, 2, 3, 10));
+        }
+    }
+}
