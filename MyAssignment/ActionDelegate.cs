e845e8f099711451eccbbea34471d3b46cf015fe
thisisdishang <dishangkumarrana@gmail.com>
2024-11-19 15:00:02 +0530

    Update ActionDelegate.cs

--- MyAssignment/ActionDelegate.cs
+++ MyAssignment/ActionDelegate.cs
@@ -6,5 +6,19 @@ namespace MyAssignment
 {
     internal class ActionDelegate
     {
+        public static void mysum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static void Main(string[] args)
+        {
+            Action<int, int> ad = mysum;
+            ad(5, 2);
+
+            Action<string> ad2 = delegate (string s) { Console.WriteLine(s); };
+            ad2("Just Bring It!");
+        }
+
     }
 }
