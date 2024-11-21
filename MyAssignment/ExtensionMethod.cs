ad9be971a290145d08074da7a62b7ef788b1cb59
thisisdishang <dishangkumarrana@gmail.com>
2024-11-21 18:05:23 +0530

    Implement Extension Method in C Sharp

---
+++ MyAssignment/ExtensionMethod.cs
@@ -0,0 +1,37 @@
+﻿// Extension method in C Sharp
+
+using System;
+
+namespace MyAssignment
+{
+    class ExtensionMethod
+    {
+        public void method1()
+        {
+            Console.WriteLine("Method 1");
+        }
+        public void method2()
+        {
+            Console.WriteLine("Method 2");
+        }
+    }
+
+    static class AnotherMethod
+    {
+        public static void method3(this ExtensionMethod e)
+        {
+            Console.WriteLine("Method 3");
+        }
+    }
+
+    class Extension
+    {
+        static void Main(string[] args)
+        {
+            ExtensionMethod e = new ExtensionMethod();
+            e.method1();
+            e.method2();
+            e.method3();
+        }
+    }
+}
