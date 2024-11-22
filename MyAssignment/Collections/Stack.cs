2bb8f22a45bebcb5fa3bab53e7377e684762cb4e
thisisdishang <dishangkumarrana@gmail.com>
2024-11-22 22:05:23 +0530

    Update Stack.cs

--- MyAssignment/Collections/Stack.cs
+++ MyAssignment/Collections/Stack.cs
@@ -1,6 +1,7 @@
 ﻿// Stack collection in C Sharp
 
 using System;
+using System.Collections.Generic;
 
 namespace MyAssignment.Collections
 {
@@ -8,7 +9,19 @@ namespace MyAssignment.Collections
     {
         static void Main(string[] args)
         {
+            Stack<string> city = new Stack<string>();
+            city.Push("Surat");
+            city.Push("Vadodara");
+            city.Push("Ahmedabad");
 
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Peek Element: " + city.Peek());
+            Console.WriteLine("Pop Element: " + city.Pop());
+            Console.WriteLine("After pop, Peek Element: " + city.Peek());
         }
     }
 }
