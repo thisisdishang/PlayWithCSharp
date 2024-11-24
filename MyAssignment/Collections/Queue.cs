2ad6bbc9f6e4ebe80578821d7d518241c6b7552f
thisisdishang <dishangkumarrana@gmail.com>
2024-11-24 15:02:11 +0530

    Update Queue.cs

--- MyAssignment/Collections/Queue.cs
+++ MyAssignment/Collections/Queue.cs
@@ -7,5 +7,22 @@ namespace MyAssignment.Collections
 {
     internal class Queue
     {
+        static void Main(string[] args)
+        {
+            Queue<int> number = new Queue<int>();
+            number.Enqueue(1);
+            number.Enqueue(2);
+            number.Enqueue(3);
+            number.Enqueue(4);
+            number.Enqueue(5);
+
+            foreach (var item in number) { 
+                Console.WriteLine(item);
+            }
+            
+            Console.WriteLine("Peek Element: "+number.Peek());
+            Console.WriteLine("Dequeue: "+number.Dequeue());
+            Console.WriteLine("After dequeue, Peek Element: " + number.Peek());
+        }
     }
 }
