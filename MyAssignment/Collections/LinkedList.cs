d870b3a9ecbc8e136f4c38444b16b6c1ceaf5001
thisisdishang <dishangkumarrana@gmail.com>
2024-11-25 18:02:12 +0530

    Implement LinkedList

---
+++ MyAssignment/Collections/LinkedList.cs
@@ -0,0 +1,23 @@
+﻿// LinkedList in C Sharp
+
+using System;
+using System.Collections.Generic;
+
+namespace MyAssignment.Collections
+{
+    internal class LinkedList
+    {
+        static void Main(string[] args)
+        {
+            var city = new LinkedList<string>();
+            city.AddFirst("Surat");
+            city.AddFirst("Vadodara");
+            city.AddLast("Ahmedabad");
+
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
