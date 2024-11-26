3af1de6624d29a7b5d435e45c957a10e5ad78f99
thisisdishang <dishangkumarrana@gmail.com>
2024-11-26 22:50:52 +0530

    Update Dictionary.cs

--- MyAssignment/Collections/Dictionary.cs
+++ MyAssignment/Collections/Dictionary.cs
@@ -9,7 +9,16 @@ namespace MyAssignment.Collections
     {
         static void Main(string[] args)
         {
+            Dictionary<int,string> student = new Dictionary<int,string>();
+            student.Add(201, "Lucifer");
+            student.Add(202, "Peter");
+            student.Add(203, "Kratos");
+            student.Add(204, "Genesis");
 
+            foreach (var item in student)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
